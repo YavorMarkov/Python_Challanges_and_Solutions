b40b497dff50392d49d2558c5af99d5b941c07a3
YavorMarkov <yavormaxpower@gmail.com>
2023-06-18 18:52:23 +0300

    edit

--- Solution_2_Shooting_day.py
+++ Solution_2_Shooting_day.py
@@ -29,27 +29,51 @@
 
 
 
-
+# We're importing the math module because we need to use the math.ceil() function later in the code.
 import math
 
-# Input
-snapshot_time = int(input())  # Read the snapshot time from the console and convert it to an integer
-number_of_scenes = int(input())  # Read the number of scenes from the console and convert it to an integer
-scene_duration = int(input())  # Read the scene duration from the console and convert it to an integer
+# This function calculates the preparation time based on the snapshot time.
+# It's a simple multiplication: snapshot_time * 0.15.
+# The snapshot_time parameter is given to the function when it's called.
+def calculate_preparation_time(snapshot_time):
+    return snapshot_time * 0.15
 
-# Calculation
-preparation_time = snapshot_time * 0.15  # Calculate the preparation time, which is 15% of the snapshot time
-total_scene_time = number_of_scenes * scene_duration  # Calculate the total time required for all the scenes
+# This function calculates the total scene time based on the number of scenes and the duration of each scene.
+# It's a simple multiplication: number_of_scenes * scene_duration.
+# Both parameters are given to the function when it's called.
+def calculate_total_scene_time(number_of_scenes, scene_duration):
+    return number_of_scenes * scene_duration
 
-total_time_needed = preparation_time + total_scene_time  # Calculate the total time needed to finish shooting the movie
-remaining_time = snapshot_time - total_time_needed  # Calculate the remaining time after subtracting the total time needed
+# This function calculates the remaining time by subtracting the total time needed from the snapshot time.
+# Both parameters are given to the function when it's called.
+def calculate_remaining_time(snapshot_time, total_time_needed):
+    return snapshot_time - total_time_needed
 
-# Output
-if remaining_time >= 0:
-    # If the remaining time is non-negative, it means there is enough time to finish the movie
-    # Use math.ceil to round up the remaining time to the nearest whole number
-    print(f"You managed to finish the movie on time! You have {math.ceil(remaining_time)} minutes left!")
-else:
-    # If the remaining time is negative, it means there isn't enough time to finish the movie
-    # Use math.ceil and abs to round up the absolute value of the remaining time to the nearest whole number
-    print(f"Time is up! To complete the movie you need {math.ceil(abs(remaining_time))} minutes.")
+# This function prints the result based on the remaining time.
+# If there's enough remaining time (remaining_time >= 0), it prints a success message.
+# If there isn't enough remaining time (remaining_time < 0), it prints a failure message.
+# The remaining_time parameter is given to the function when it's called.
+def print_result(remaining_time):
+    if remaining_time >= 0:
+        print(f"You managed to finish the movie on time! You have {math.ceil(remaining_time)} minutes left!")
+    else:
+        print(f"Time is up! To complete the movie you need {math.ceil(abs(remaining_time))} minutes.")
+
+# Input section of the code.
+# We're reading the snapshot time, number of scenes, and scene duration from the console.
+# The int() function is used to convert these inputs to integers since input() only reads strings.
+snapshot_time = int(input())
+number_of_scenes = int(input())
+scene_duration = int(input())
+
+# Calculation section of the code.
+# We're calling our functions here with the appropriate arguments.
+# The results of these function calls are stored in variables.
+preparation_time = calculate_preparation_time(snapshot_time)
+total_scene_time = calculate_total_scene_time(number_of_scenes, scene_duration)
+total_time_needed = preparation_time + total_scene_time
+remaining_time = calculate_remaining_time(snapshot_time, total_time_needed)
+
+# Output section of the code.
+# We're calling our print_result() function with the remaining_time variable.
+print_result(remaining_time)
